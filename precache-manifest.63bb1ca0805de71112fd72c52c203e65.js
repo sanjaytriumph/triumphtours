87d3369b8e2e27b9d247005b93ee81f224f2f183
Ajay <aryaajay069@gmail.com>
2022-07-14 20:13:51 +0530

    Updates

---
+++ precache-manifest.63bb1ca0805de71112fd72c52c203e65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a9de5b6eec91fb3b04d9688296bbc2",
+    "url": "/triumphtours/index.html"
+  },
+  {
+    "revision": "208d7aad88d49684a833",
+    "url": "/triumphtours/static/css/main.c3e53c0d.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/triumphtours/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/triumphtours/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "208d7aad88d49684a833",
+    "url": "/triumphtours/static/js/main.c8165ae2.chunk.js"
+  },
+  {
+    "revision": "0dffff1c3b33045bbf4e",
+    "url": "/triumphtours/static/js/runtime-main.ff4b4223.js"
+  }
+]);
